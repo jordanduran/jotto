1cf9467425aa9984bc24ebd348052520bd233b95
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2022-01-05 15:33:38 -0500

    Initial test setup for congrats component

---
+++ src/Congrats.js
@@ -0,0 +1,12 @@
+/*
+ * Functional react component for congratulatory message
+ * @function
+ * @param {object} props - React props.
+ * @returns {JSX.Element} - Rendered component (or null if 'success' prop is false)
+ */
+
+const Congrats = () => {
+  return <div></div>;
+};
+
+export default Congrats;
