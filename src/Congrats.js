174abf6bcb0186a29d313a88ea93d42c8839cd02
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2022-01-05 15:54:28 -0500

    Passing tests for congrats component

--- src/Congrats.js
+++ src/Congrats.js
@@ -5,8 +5,18 @@
  * @returns {JSX.Element} - Rendered component (or null if 'success' prop is false)
  */
 
-const Congrats = () => {
-  return <div></div>;
+const Congrats = (props) => {
+  if (props.success) {
+    return (
+      <div data-test='component-congrats'>
+        <span data-test='congrats-message'>
+          Congratulations! You guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test='component-congrats'></div>;
+  }
 };
 
 export default Congrats;
